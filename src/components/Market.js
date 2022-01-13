37f35a6314ebc31afbd0672d32dc05d04137ca40
Sae-byeol <toquf0424@naver.com>
2022-01-13 13:59:29 +0900

    feat: 페이지네이션 기능 구현

--- src/components/Market.js
+++ src/components/Market.js
@@ -4,26 +4,46 @@ import Header from './Header';
 import '../Market.css';
 import MarketForm from './MarketForm';
 import {BrowserRouter, Route, Routes,Link} from 'react-router-dom';
+import "../paging.css";
+import Pagination from "react-js-pagination";
 
 const Market = () => {
     const [markets, setMarkets]=useState([
         {title:'흰 블라우스', img:'../img/test.png', no:1 },
         {title:'흰 블라우스', img:'../img/test.png', no:2 },
         {title:'흰 블라우스', img:'../img/test.png', no:3 },
-        {title:'흰 블라우스', img:'../img/test.png', no:4 }
-    ]
-        
-    )
-    const renderMarkets=markets.map(market=>{
+        {title:'흰 블라우스', img:'../img/test.png', no:4 },
+        {title:'흰 블라우스', img:'../img/test.png', no:5 },
+        {title:'흰 블라우스', img:'../img/test.png', no:6 },
+        {title:'흰 블라우스', img:'../img/test.png', no:7 },
+        {title:'흰 블라우스', img:'../img/test.png', no:8 },
+        {title:'흰 블라우스', img:'../img/test.png', no:9 },
+        {title:'흰 블라우스', img:'../img/test.png', no:10 },
+        {title:'흰 블라우스', img:'../img/test.png', no:11},
+        {title:'흰 블라우스', img:'../img/test.png', no:12},
+    ])
+    const [page, setPage] = useState(1);
+    const onClicksetPage = markets.filter((market) => {
+        return (page - 1) * 9 + 1 <= market.no && market.no <= (page - 1) * 9 + 9;
+      });
+
+    const renderMarkets = onClicksetPage.map((market) => {
         return (
-           
-            <MarketForm market={market} key={market.no}></MarketForm>
-            
-                
-            
-            
-        )
-    })
+          <div className='market-flex'>
+            <MarketForm
+              market={market}
+              key={market.no}
+              title={market.title}
+              id={market.id}
+            ></MarketForm>
+          </div>
+        );
+      });
+      const handlePageChange = (page) => {
+        setPage(page);
+        onClicksetPage();
+        renderMarkets();
+      };
     
     return (
         <div className='App'>
@@ -45,8 +65,22 @@ const Market = () => {
                 <div className='market-section2'>
                     {renderMarkets}
                 </div>
+                <div>
+                    <Pagination
+                    activePage={page}
+                    itemsCountPerPage={5}
+                    totalItemsCount={
+                        parseInt(markets.length % 9) === 0
+                        ? parseInt(markets.length / 9) * 5
+                        : (parseInt(markets.length / 9) + 1) * 5
+                    }
+                    pageRangeDisplayed={5}
+                    prevPageText={"<"}
+                    nextPageText={">"}
+                    onChange={handlePageChange}
+                    />
+                </div>
             </div>
-            
         </div>
     )
 }
