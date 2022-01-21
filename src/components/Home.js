0834b7395f9f66a9508aca72e804d079a7a7f5bd
Sae-byeol <toquf0424@naver.com>
2022-01-22 02:32:10 +0900

    style: 메인페이지 ui만

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,14 +8,27 @@ import axios from 'axios';
 import { asRoughMinutes } from '@fullcalendar/react';
 
 const Home = () => {
-    const [notice, setNotice]=useState([]);
+    const [notice, setNotice]=useState([
+        {title:"notice1", date: "2022-01-03"},
+        {title:"notice2", date: "2022-01-03"},
+        {title:"notice3", date: "2022-01-03"},
+        {title:"notice4", date: "2022-01-03"},
+    ]);
+    const [newPost, setNewPost]=useState([
+        {from:"추천게시판", title:"newPost1", date:"2021.10.31 19:11"},
+        {from:"추천게시판", title:"newPost1", date:"2021.10.31 19:11"},
+        {from:"추천게시판", title:"newPost1", date:"2021.10.31 19:11"},
+        {from:"추천게시판", title:"newPost1", date:"2021.10.31 19:11"},
+        {from:"추천게시판", title:"newPost1", date:"2021.10.31 19:11"},
+    ])
     const [events, setEvents]=useState([]);
-
+    
     useEffect(() => {
         axios.get('dummy/calendar_list.json')
         .then(res=>setEvents(res.data.calendarList))
        .catch(err=>console.log(err));
        }, []);
+
 
        
    
@@ -44,8 +57,35 @@ const Home = () => {
                     <img className='home-NoticeImg' src="../img/home3.png"></img>
                     <span className='home-NoticeText'>관리사무소에서 알립니다!<br/>이번 달의 공지사항을 확인하세요!</span>
                     <div className='home-notice'>
+                        {notice.map((val)=>{
+                            return (
+                                <div key={val.index} className='box'>
+                                    <div className='home-notice-map'>
+                                        <div className='home-notice-map-title'>{val.title}</div>
 
+                                        <div className='home-notice-map-date'>{new Date(val.date).toLocaleDateString()}</div>
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </div>
+            </div>
+            <div className='home-section3'>
+                <div className='home-best-title'>최신 추천글</div>
+                <div className='home-best'>
+                    {newPost.map((val)=>{
+                        return(
+                            <div key={val.index} className='box'>
+                            <div className='home-best-map'>
+                                <div className='home-best-map-from'>{val.from}</div>
+                                <div className='home-best-map-title'>{val.title}</div>
+                                {/* date는 toLocaleString() 사용하기*/}
+                                <div className='home-notice-map-date'>{val.date}</div>
+                            </div>
+                        </div>
+                        )
+                    })}
+                </div>
             </div>
 
             </div>
