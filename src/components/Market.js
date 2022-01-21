c01eebeb05de2e7bca042afcf980290a0c5f6991
Sae-byeol <toquf0424@naver.com>
2022-01-21 13:17:11 +0900

    feat: 검색 기능 구현

--- src/components/Market.js
+++ src/components/Market.js
@@ -10,12 +10,6 @@ import Pagination from "react-js-pagination";
 const Market = (props) => {
     const markets=props.component;
     const [page, setPage] = useState(1);
-    const [renderPage, setRenderPage] = useState("unfocused");
-   // const onClicksetPage = markets.filter((market) => {
-   //     return (page - 1) * 9 + 1 <= market.usedGoods_id && market.usedGoods_id <= (page - 1) * 9 + 9;
-   //   });
-   const [buttonColor, setButtonColor] = useState("all");
-   const [getName, setGetName] = useState("");
    const [search, setSearch] = useState(null);
    const [searchingText, setSearchingText] = useState(null);
  
@@ -43,91 +37,14 @@ const Market = (props) => {
      return market;
    });
  
-   // unfocused 상태일 때 각 페이지에 보여줄 객체들 필터
+  
    const onClicksetPage = BeforeonClicksetPage.filter((market) => {
      return (
        (page - 1) * 9 + 1 <= market.usedGoods_id && market.usedGoods_id<= (page - 1) * 9 + 9
      );
    });
- 
-   // unfocused 상태일 때 보여줄 객체들 BestForm 형태로 나타내기
- 
-   // unfocused 상태일 때
-   // 처음에 보여주는 페이지는 1 페이지
-   // focused -> unfocused 되기
-   // 위의 함수들 실행
-   const onBlurButton = () => {
-     setPage(1);
-     setRenderPage("unfocused");
-     BeforeonClicksetPage();
-     onClicksetPage();
-     renderMarkets();
-     return null;
-   };
- 
-   // 버튼을 클릭했을 때
-   // unfocusesd -> focused
-   // 버튼의 name 가져온다
-   const onClickButtonGetName = (e) => {
-     let getName = "";
-     setRenderPage("focused");
-     return setGetName(getName.concat(e));
-   };
- 
-   // name과 케테고리가 일치하는 것만 필터링
-   const onClickButtonClassify = searchedMarkets.filter((market) => {
-     //return be.category === getName;
-   });
- 
-   // bestNo 값 재지정
-   let i = 1;
-   const onClickButtonSetForm = onClickButtonClassify.map((market) => {
-    market.usedGoods_id = i;
-     i++;
-     return market;
-   });
- 
-   // bestNo 값에 따라 페이지별로 보여줄 객체들 필터링
-   const onClickButtonsetPage = onClickButtonSetForm.filter((market) => {
-     return (
-       (page - 1) * 9 + 1 <= market.usedGoods_id && market.usedGoods_id  <= (page - 1) * 9 + 9
-     );
-   });
- 
-   // focused 상태일 때 보여줄 객체들 BestForm 형태로 나타내기
-   const onClickButtonrenderMarkets = onClickButtonsetPage.map((market) => {
-     return (
-       <div className="best-flex">
-         <MarketForm
-           market={market}
-           key={market.usedGoods_id}
-           title={market.title}
-           id={market.usedGoods_id}
-         ></MarketForm>
-       </div>
-     );
-   });
- 
-   // 버튼 클릭하면
-   // 위 함수들 실행
-   // 처음에 보옂는 페이지는 1 페이지
-   const onClickButton = (e) => {
-     setPage(1);
-     onClickButtonGetName(e);
-     onClickButtonClassify();
-     onClickButtonSetForm();
-     onClickButtonsetPage();
-     return null;
-   };
- 
+
    const handlePageChange = (page) => {
-     setRenderPage("unfocused");
-     setPage(page);
-     window.scrollTo(0, 0);
-   };
- 
-   const FocusedHandlePageChange = (page) => {
-     setRenderPage("focused");
      setPage(page);
      window.scrollTo(0, 0);
    };
@@ -180,7 +97,7 @@ const Market = (props) => {
                 </div>
                 <div className='line'></div>
                 <div className='market-section2'>
-                {renderPage === "unfocused" ? renderMarkets : onClickButtonrenderMarkets}
+                {renderMarkets}
                 </div>
                 <div>
                     <Pagination
