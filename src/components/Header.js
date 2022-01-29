8d21f4445981ad21826c2f8de0bc8c21b0cb2164
yulleta <mg09091234@gmail.com>
2022-01-30 00:28:37 +0900

    style: header 쪽지함 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,33 @@
-import React from 'react'
-import '../Header.css' 
-import {BrowserRouter, Route, Routes,Link,Outlet} from 'react-router-dom';
+import React from "react";
+import "../Header.css";
+import { BrowserRouter, Route, Routes, Link, Outlet } from "react-router-dom";
 const Header = () => {
-    return (
-        <div className='header'>
-          <Link to="/">
-          <div className='header-left'>
-            <img className='imagelogo' src='../img/imagelogo.png' width="100" height="100"></img>
-            <img className='logo' src='../img/logo.png'></img>
-          </div>
-          </Link>
-          <div className='header-right'>
-            <div className='header-right-left'>
-              <div>나의 쪽지함</div>
-              <div>오새별님</div>
-            </div>
-            <div className='header-right-right'>
-            <Link to="/mypage">
-            <img className="profile" src='../img/profile.png'></img>
-            </Link>
-            </div>
-            
-          </div>   
+  return (
+    <div className="header">
+      <Link to="/">
+        <div className="header-left">
+          <img
+            className="imagelogo"
+            src="../img/imagelogo.png"
+            width="100"
+            height="100"
+          ></img>
+          <img className="logo" src="../img/logo.png"></img>
         </div>
-    )  
-}
+      </Link>
+      <div className="header-right">
+        <div className="header-right-left">
+          <button className="header-note">나의 쪽지함</button>
+          <div>오새별님</div>
+        </div>
+        <div className="header-right-right">
+          <Link to="/mypage">
+            <img className="profile" src="../img/profile.png"></img>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
