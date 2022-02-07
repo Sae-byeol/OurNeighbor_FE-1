d2d3373a86b98c495b823909c979387757c78223
Sae-byeol <toquf0424@naver.com>
2022-02-07 19:10:33 +0900

    style: 중고거래 ui 수정

--- src/components/MarketPostView.js
+++ src/components/MarketPostView.js
@@ -236,17 +236,6 @@ const MarketPostView = () => {
           parseInt(market.marketNo) + 2
         );
 
-  // 카테고리 이름 보여주는 함수
-  const categoryName = () => {
-    if (market.category === "food") return "맛집";
-
-    if (market.category === "academy") return "학원";
-
-    if (market.category === "cafe") return "카페";
-
-    if (market.category === "sports") return "운동시설";
-  };
-
   // <대댓글 구현>
   const [commentContents, setCommentContents] = useState("");
   const [commentList, setCommentList] = useState([]);
@@ -359,7 +348,7 @@ const MarketPostView = () => {
         <Header></Header>
         <Navbar></Navbar>
         <div className="section1">
-          <span className="sub-title1">추천게시판</span>
+          <span className="sub-title1">중고거래</span>
         </div>
         <div className="line"></div>
         <div className="marketPostView-section1">
@@ -393,10 +382,7 @@ const MarketPostView = () => {
           </div>
         </div>
         <div className="category-line"></div>
-        <div className="category-name">카테고리</div>
-        <div className="category">
-          <span className="category-type">{categoryName()}</span>
-        </div>
+        
         <div className="relpy-line"></div>
         <div className="marketPostView-section2">
           <div className="reply-title">댓글</div>
